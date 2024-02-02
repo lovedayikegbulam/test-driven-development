a00ab590e41ee3a09fdcae9cc6ab3d4687e651f3
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-02-02 23:16:22 +0100

    update test description for each test

--- test/math.test.js
+++ test/math.test.js
@@ -4,15 +4,15 @@ test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
+test('subtract 2 - 1 to equal 1', () => {
   expect(sub(2, 1)).toBe(1);
 });
 
-test('adds 1 + 2 to equal 3', () => {
+test('divide 4 / 2 to equal 2', () => {
   expect(div(4, 2)).toBe(2);
 });
 
-test('adds 1 + 2 to equal 3', () => {
+test('exponent 2 ** 2 to equal 4', () => {
   expect(pow(2, 2)).toBe(4);
 });
 
